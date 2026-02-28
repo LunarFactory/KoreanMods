9ae39a1d9d7e7ca7b29d468ef1cdfbd4ac5f9fd1
PixVoxel <wnsdl1247@gmail.com>
2026-03-01 03:23:17 +0900

    Updated Translation

--- language/MiscFixes/english.lua
+++ language/MiscFixes/english.lua
@@ -1,5 +1,19 @@
 return {
     item = {
-        keycard = { description = "Can unlock <b>the locked door</c> on specific stage." },
+        keycard = { description = "Interacted with while held. Consumed on use." },
+        whiteUndershirt = { description = "<g>Increase Armor</c> by <g>3</c> <c_stack>(+3 per stack)</c>." },
+        smallEnigma = { description = "Reduce <b>Equipment Cooldown</c> by <b>5%</c> <c_stack>(+5% per stack)</c>." },
+        strangeBattery = { description = "Fire <y>chain lightning</c> for <y>33% damage</c>." },
+        playerScrap1 = { description = "Really?" },
+        playerScrap2 = { description = "This thing has no use." },
+        playerScrap3 = { description = "Useless." },
+        playerScrap4 = { description = "How does it happen?" },
+        jellySalad = { description = "Reduces <b>skill cooldowns</c> by <b>1 second</c> <c_stack>(+0.5s per stack)</c>. Cooldowns cannot go below <b>0.5s</c>." },
+        marinatedLemurian = { description = "Increase <y>damage</c> by <y>20%</c> <c_stack>(+10% per stack)</c>." },
+        golemBeer = { description = "Gain <g>50% max HP</c> as <g>barrier</c> on pickup, and <b>40 armor</c> <c_stack>(+10 per stack)</c>." },
+        friedEyeball = { description = "Slow enemy <b>movement speed</c> by <b>37.5%</c> and <y>attack speed</c> by <y>25%</c> in an area around you <c_stack>(stacks increase range)</c>." },
+        saturnianSteak = { description = "Increase <g>health regeneration</c> by <g>0.6 HP/s</c> <c_stack>(+0.6 per stack)</c>, and movement speed by <b>2km/h</c> <c_stack>(+2 per stack)</c>." },
+        diosFriendUsed = { description = "Useless." },
+        timeKeepersSecretUsed = { description = "Useless." }
     },
 }
