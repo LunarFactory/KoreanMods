fb93791eb9fae6c6629837bef0f68dba8c0f9c88
PixVoxel <wnsdl1247@gmail.com>
2026-03-01 15:39:12 +0900

    Updated Translations

--- language/MiscFixes/english.lua
+++ language/MiscFixes/english.lua
@@ -8,11 +8,13 @@ return {
         playerScrap2 = { description = "This thing has no use." },
         playerScrap3 = { description = "Useless." },
         playerScrap4 = { description = "How does it happen?" },
+
         jellySalad = { description = "Reduces <b>skill cooldowns</c> by <b>1 second</c> <c_stack>(+0.5s per stack)</c>. Cooldowns cannot go below <b>0.5s</c>." },
         marinatedLemurian = { description = "Increase <y>damage</c> by <y>20%</c> <c_stack>(+10% per stack)</c>." },
         golemBeer = { description = "Gain <g>50% max HP</c> as <g>barrier</c> on pickup, and <b>40 armor</c> <c_stack>(+10 per stack)</c>." },
         friedEyeball = { description = "Slow enemy <b>movement speed</c> by <b>37.5%</c> and <y>attack speed</c> by <y>25%</c> in an area around you <c_stack>(stacks increase range)</c>." },
         saturnianSteak = { description = "Increase <g>health regeneration</c> by <g>0.6 HP/s</c> <c_stack>(+0.6 per stack)</c>, and movement speed by <b>2km/h</c> <c_stack>(+2 per stack)</c>." },
+        
         diosFriendUsed = { description = "Useless." },
         timeKeepersSecretUsed = { description = "Useless." }
     },
